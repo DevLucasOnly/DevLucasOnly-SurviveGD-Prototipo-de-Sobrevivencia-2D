b75ec225cf6fafc589b3c66799b429833281d371
Lucas <l.arribada123@gmail.com>
2026-09-01 20:26:48 -0300

    Adicionei animação ao movimento e a camera acompanha o personagem

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,19 +4,28 @@ extends CharacterBody2D
 @export var humanidade: float = 100.0
 @export var taxa_decaimento: float = 5.0
 
+@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+
 func _physics_process(delta: float) -> void:
-	# 1. Verifica estado vital
 	if humanidade > 0.0:
-		# 2. Aplica o decaimento contínuo baseado no tempo (delta)
 		humanidade -= taxa_decaimento * delta
 		
-		# 3. Trava o movimento imediatamente se zerar
 		if humanidade <= 0.0:
 			humanidade = 0.0
-			print("Humanidade esgotada. Movimento bloqueado.")
+			anim.play("idle")
 			return 
 
-		# 4. Cálculo vetorial normalizado (executado apenas se humanidade > 0)
 		var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 		velocity = input_direction * speed
+		
+		# Controle de Animação
+		if velocity != Vector2.ZERO:
+			anim.play("walk")
+			# Inverte o sprite horizontalmente se estiver indo para a esquerda
+			if velocity.x != 0:
+				sprite.flip_h = velocity.x < 0
+		else:
+			anim.play("idle")
+			
 		move_and_slide()
