a0fe71f3186e98f78057f00060a4018139dd8665
Lucas <l.arribada123@gmail.com>
2026-09-09 20:17:29 -0300

    Ajuste no player.gd e antidoto.gd para melhor funcionamento com o Game Manager

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,34 @@
 extends CharacterBody2D
 
 @export var speed: float = 300.0
-@export var humanidade: float = 100.0
-@export var taxa_decaimento: float = 5.0
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 
-func _physics_process(delta: float) -> void:
-	if humanidade > 0.0:
-		humanidade -= taxa_decaimento * delta
-		
-		if humanidade <= 0.0:
-			humanidade = 0.0
-			anim.play("idle")
-			return 
+func _ready() -> void:
+	# Conecta o sinal do Autoload a uma função local para validação
+	GameManager.humanidade_alterada.connect(_on_humanidade_alterada)
 
+func _physics_process(_delta: float) -> void:
+	# Verifica o estado no script global
+	if GameManager.humanidade_atual > 0.0:
 		var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 		velocity = input_direction * speed
 		
 		# Controle de Animação
 		if velocity != Vector2.ZERO:
 			anim.play("walk")
-			# Inverte o sprite horizontalmente se estiver indo para a esquerda
 			if velocity.x != 0:
 				sprite.flip_h = velocity.x < 0
 		else:
 			anim.play("idle")
 			
-		move_and_slide()
+	else:
+		# Bloqueia a movimentação e força animação base caso a energia zere
+		velocity = Vector2.ZERO
+		anim.play("idle")
+		
+	move_and_slide()
+
+func _on_humanidade_alterada(valor: float) -> void:
+	print("Humanidade: ", valor)
