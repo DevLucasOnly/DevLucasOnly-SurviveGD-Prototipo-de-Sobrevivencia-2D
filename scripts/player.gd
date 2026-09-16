351602da718682aa6da6bef5432cabfb638045d4
Lucas <l.arribada123@gmail.com>
2026-09-16 15:58:35 -0300

    Ajuste no player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,6 +14,9 @@ func _ready() -> void:
 	GameManager.game_over.connect(_on_game_over)
 
 func _physics_process(_delta: float) -> void:
+
+	print("Estado atual: ", estado_atual)
+
 	match estado_atual:
 		Estado.IDLE:
 			_estado_idle()
