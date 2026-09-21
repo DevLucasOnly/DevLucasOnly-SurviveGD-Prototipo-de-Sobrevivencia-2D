e9c30f020f8d4f7747ff8abe03264a5cda0ed594
Lucas <l.arribada123@gmail.com>
2026-09-21 12:22:43 -0300

    Ajuste no script de menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	$BtnJogar.pressed.connect(_on_jogar_pressed)
 
 func _on_jogar_pressed() -> void:
-	get_tree().change_scene_to_file(res://scenes/world.tscn)
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
