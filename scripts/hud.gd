3ce6e3cc287adcc6bd55d9fe8ed5514fc286a5d0
Lucas <l.arribada123@gmail.com>
2026-09-24 21:25:21 -0300

    Ajuste na barra vida e humanidade (ERRO)

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,22 +1,31 @@
 extends CanvasLayer
 
-@onready var barra_humanidade: ProgressBar = $BarraHumanidade
+@onready var barra_humanidade: TextureProgressBar = $BarraHumanidade
+@onready var barra_vida: TextureProgressBar = $BarraVida
 @onready var tempo_label: Label = $TempoLabel
 
 func _ready() -> void:
-	# Inicializa Barra
+	# Inicializa Humanidade
 	barra_humanidade.max_value = GameManager.humanidade_maxima
 	barra_humanidade.value = GameManager.humanidade_atual
+	GameManager.humanidade_alterada.connect(_atualizar_humanidade)
 	
-	# Conexões restritas ao funcionamento do HUD
-	GameManager.humanidade_alterada.connect(_atualizar_barra)
+	# Inicializa Vida
+	barra_vida.max_value = GameManager.vida_maxima
+	barra_vida.value = GameManager.vida_atual
+	GameManager.vida_alterada.connect(_atualizar_vida)
+	
+	# Inicializa Tempo
 	GameManager.tempo_atualizado.connect(_atualizar_tempo)
 
-func _atualizar_barra(valor: float) -> void:
+func _atualizar_humanidade(valor: float) -> void:
 	barra_humanidade.value = valor
 
+# CORREÇÃO: O parâmetro 'valor' agora é int, correspondendo ao sinal do GameManager
+func _atualizar_vida(valor: int) -> void:
+	barra_vida.value = valor
+
 func _atualizar_tempo(tempo_segundos: int) -> void:
-	# Formata e atualiza o relógio
 	var minutos = int(tempo_segundos / 60.0) 
 	var segundos = tempo_segundos % 60
 	tempo_label.text = "%02d:%02d" % [minutos, segundos]
